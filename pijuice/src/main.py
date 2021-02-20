6d843c3f2aa217078e121c5d4f7243311fd0e455
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-02-20 12:26:34 +0000

    Update main.py

--- pijuice/src/main.py
+++ pijuice/src/main.py
@@ -1,7 +1,6 @@
 from time import sleep
 import datetime
 import sys, getopt, os
-sys.path.append('/usr/lib/python3.5/dist-packages') # temporary hack to import the piJuice module
 from pijuice import PiJuice
 from balena import Balena
 from twilio.rest import Client
