4507350a1e025326ebe6353de03cd3d35efa22ae
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-03-04 16:38:01 +0000

    power button

---
+++ buttons/button.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#!
+import RPi.GPIO as GPIO
+import time
+import pydbus
+import gi
+
+# Set GPIO mode: GPIO.BCM or GPIO.BOARD
+GPIO.setmode(GPIO.BOARD)
+
+# Set pin 5 an an input, and enable the internal pull-up resistor
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+oldButtonState1 = True
+
+while True:
+    buttonState1 = GPIO.input(5)
+
+    if buttonState1 != oldButtonState1 and buttonState1 == False :
+        bus = pydbus.SystemBus()
+        logind = bus.get('.login1')['.Manager']
+        logind.PowerOff(True)
+
+    oldButtonState1 = buttonState1
+
+time.sleep(1)
