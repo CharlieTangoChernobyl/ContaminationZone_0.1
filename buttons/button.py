284b64d1840d376650d256ecfe87500b6a1072f3
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-03-04 21:10:47 +0000

    Update button.py

--- buttons/button.py
+++ buttons/button.py
@@ -14,7 +14,7 @@ GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 oldButtonState1 = True
 
 while True:
-    buttonState1 = GPIO.input(5)
+    buttonState1 = GPIO.input(7)
 
     if buttonState1 != oldButtonState1 and buttonState1 == False :
         bus = pydbus.SystemBus()
