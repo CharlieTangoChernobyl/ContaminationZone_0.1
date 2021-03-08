e7731a3173b75f18d31875686a50cd0bfa1c49cc
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-03-08 21:56:16 +0000

    Update shutdown.py

--- buttons/shutdown.py
+++ buttons/shutdown.py
@@ -5,10 +5,12 @@ import datetime
 import RPi.GPIO as GPIO
 import smbus
 
+GPIO.setmode(GPIO.BOARD)
+
 # This method fires on edge detection (the pulse from the counter board)
 def countme(channel):
     os.system('shutdown -h now')
 
 # Set the input with falling edge detection for geiger counter pulses
-GPIO.setup(26, GPIO.IN)
-GPIO.add_event_detect(26, GPIO.FALLING, callback=countme)
+GPIO.setup(37, GPIO.IN)
+GPIO.add_event_detect(37, GPIO.FALLING, callback=countme)
